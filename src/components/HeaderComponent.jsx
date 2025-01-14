ee65b80800a384bca18ff1596cb280351e87a620
alessiotinaglia <alessio.tina1998@gmail.com>
2025-01-14 12:21:56 +0100

    fine nav

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,19 +1,35 @@
-import { NavLink } from "react-router-dom";
-
 function Header() {
     return (
         <header>
-            <ul className="nav nav-pills p-3">
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/HomePage">Homepage</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/posts">Lista delle ricette</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/Contatti">Contatti</NavLink>
-                </li>
-            </ul>
+            <nav className="navbar navbar-expand-lg navbar-dark">
+                <div className="container-fluid">
+                <img src="/download.jpg" alt="Medusa" width="45" height="60" /> 
+                    <strong>
+                        <a className="navbar-brand text-danger fs-1">MEDUSA</a>
+                    </strong>
+
+                    <ul className="navbar-nav d-flex align-items-center">
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#home">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#about">Serie TV</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#contact">Film</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link text-white" href="#contact">La mia lista</a>
+                        </li>
+                    </ul>
+
+                    {/* Form di ricerca */}
+                    <form className="d-flex ms-auto" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Cerca</button>
+                    </form>
+                </div>
+            </nav>
         </header>
     );
 }
