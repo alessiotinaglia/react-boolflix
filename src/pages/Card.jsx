d8e9489085c681b47a939c0d9b8509111d0a2637
alessiotinaglia <alessio.tina1998@gmail.com>
2025-01-14 13:44:00 +0100

    richiamate con api le card

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,23 +1,56 @@
-
+import { useState, useEffect } from 'react';
 
 function Card() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const fetchMovies = () => {
+            const url = 'https://api.themoviedb.org/3/discover/movie?api_key=e99307154c6dfb0b4750f6603256716d&language=it-IT';
+
+            fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+                    setMovies(data.results);
+                })
+                .catch((error) => {
+                    console.error('Errore durante il recupero dei film:', error);
+                });
+        };
+
+        fetchMovies();
+    }, []);
+
     return (
-        <section className="container">
+        <section className="m-20 p-5 mr-5">
             <div className="row">
-                <div className="card" style={{ width: '18rem' }}>
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">
-                            Some quick example text to build on the card title and make up the bulk of the card's content.
-                        </p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                <div className="col-12 mt-6">
+                    <div className="row">
+                        {movies.length > 0 ? (
+                            movies.map((movie) => (
+                                <div className="col-md-2" key={movie.id}>
+                                    <div className="card m-3" style={{ width: '18rem' }}>
+                                        <img
+                                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                            className="card-img-top"
+                                            alt={movie.title}
+                                        />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{movie.title}</h5>
+                                            <p className="card-text">
+                                                {movie.overview ? movie.overview : 'Descrizione non disponibile'}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <div>Nessun film trovato.</div>
+                        )}
                     </div>
                 </div>
             </div>
-
         </section>
     );
-};
+}
 
 export default Card;
