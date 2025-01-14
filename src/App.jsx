00f5c8ac7a847135ea3211f7bd6e405498621036
alessiotinaglia <alessio.tina1998@gmail.com>
2025-01-14 12:57:08 +0100

    impostata card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DefaultLayout from './pages/DefaultLayout';
+import Card from './pages/Card';
 
 function App() {
   return (    
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Card />} />
           </Route>
         </Routes>
       </BrowserRouter>
