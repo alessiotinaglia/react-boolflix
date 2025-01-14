db54809e5d130be8788487b0c9082cdf5d63bfca
alessiotinaglia <alessio.tina1998@gmail.com>
2025-01-14 15:34:49 +0100

    sistemazione card

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios'; // Importa axios
+import axios from 'axios';
 
 function Card() {
     const [movies, setMovies] = useState([]);
@@ -13,22 +13,22 @@ function Card() {
                     setMovies(response.data.results);
                 })
                 .catch((error) => {
-                    console.error('Errore durante il recupero dei film:', error); 
+                    console.error('Errore durante il recupero dei film:', error);
                 });
         };
 
         fetchMovies();
-    }, []); 
+    }, []);
 
     return (
-        <section className="m-20 p-5 mr-5">
+        <main className="m-20 p-4">
             <div className="row">
                 <div className="col-12 mt-6">
                     <div className="row">
                         {movies.length > 0 ? (
                             movies.map((movie) => (
                                 <div className="col-md-2" key={movie.id}>
-                                    <div className="card m-3" style={{ width: '18rem' }}>
+                                    <div className="card m-3 movie-card" style={{ width: '18rem' }}>
                                         <img
                                             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                                             className="card-img-top"
@@ -36,9 +36,7 @@ function Card() {
                                         />
                                         <div className="card-body">
                                             <h5 className="card-title">{movie.title}</h5>
-                                            <p className="card-text">
-                                                {movie.overview ? movie.overview : 'Descrizione non disponibile'}
-                                            </p>
+                                            <p className="card-text">{movie.overview ? movie.overview : 'Descrizione non disponibile'}</p>
                                         </div>
                                     </div>
                                 </div>
@@ -49,7 +47,7 @@ function Card() {
                     </div>
                 </div>
             </div>
-        </section>
+        </main>
     );
 }
 
